fa6b920d2e54a2462e72441578c2632378767359
RyanParson <149078913+RyanParson@users.noreply.github.com>
2026-04-29 09:50:08 -0400

    Fixed Level Gen

--- Assets/Code/Level Generator.cs
+++ Assets/Code/Level Generator.cs
@@ -8,8 +8,9 @@ public class LevelGenerator : MonoBehaviour
     int randomIndex = -1;
     int previousIndex = -1;
     Transform startTransform;
-    Transform endTransform;
-    public float currentDistance = 35f;
+    [SerializeField] Transform endTransform;
+    public Vector3 currentDistance = new Vector3(0,0,35f);
+    public int RollCount = 2;
 
     // Start is called before the first frame update
     void Start()
@@ -27,16 +28,19 @@ public class LevelGenerator : MonoBehaviour
     {
         for (int i = 0; i < cellCount; i++)
             {   
-                randomIndex = Random.Range(0, levelParts.Length);
-                if (previousIndex == randomIndex)
+                randomIndex = Random.Range(0, levelParts.Length); //choses a level part at random
+
+                for (int r = 0; r < RollCount; r++) //decides how many times to roll for a new level part, to avoid dupes
                 {
-                    randomIndex = (randomIndex + 1) % levelParts.Length;
+                    if (previousIndex == randomIndex) //if its a dupe of the last one, roll again
+                    {
+                        randomIndex = Random.Range(0, levelParts.Length); //rerolls for a new level part
+                    }
                 }
-                GameObject levelPart = Instantiate(levelParts[randomIndex]);
-                endTransform = levelPart.transform.Find("end");
-                startTransform = levelPart.transform.Find("start");
-                currentDistance += endTransform.position.z - startTransform.position.z;
-                levelPart.transform.position = new Vector3(0, 0, currentDistance);
+
+                GameObject levelPart = Instantiate(levelParts[randomIndex], currentDistance, Quaternion.Euler(0,0,0)); // creates the level part at the current distance
+                endTransform = levelPart.transform.Find("End");
+                currentDistance = endTransform.position; // this is in GLOBAL space so it DOESNT need to be +=
                 previousIndex = randomIndex;
             }
     }
