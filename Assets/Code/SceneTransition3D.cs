83c85ae428f255a72f00a98906efacef572b4701
RyanParson <149078913+RyanParson@users.noreply.github.com>
2026-05-13 09:14:26 -0400

    did stuff

--- Assets/Code/SceneTransition3D.cs
+++ Assets/Code/SceneTransition3D.cs
@@ -5,7 +5,10 @@ using UnityEngine.SceneManagement;
 
 public class SceneTransition3D : MonoBehaviour
 {
+    public PlayerSettings livesInfo;
     public string sceneName;
+    public string gameOverScene;
+    public bool isDeadly;
     // Start is called before the first frame update
     void Start()
     {
@@ -21,9 +24,23 @@ public class SceneTransition3D : MonoBehaviour
     void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
-        {
-            UnityEngine.SceneManagement.SceneManager.LoadScene(sceneName);
-            print("Scene Transitioned to " + sceneName);
+        {   
+            if (isDeadly)
+            {
+                livesInfo.currentLives -= 1;
+                if (livesInfo.currentLives <= 0)
+                {
+                    UnityEngine.SceneManagement.SceneManager.LoadScene(gameOverScene);
+                }
+                else
+                {
+                    UnityEngine.SceneManagement.SceneManager.LoadScene(sceneName);
+                }
+            }
+            else
+            {
+                UnityEngine.SceneManagement.SceneManager.LoadScene(sceneName);
+            }
         }
     }
 }
